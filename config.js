605939c1d51c1eec3d955f253722f038777e5209
tester248 <74985969+tester248@users.noreply.github.com>
2023-07-07 17:17:46 +0000

    ssd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "httphandler.deezgames.workers.dev",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYzFmODcxMmE4MTMzM2Y5MDVjZmE0ZGVjNjhlY2JlNiIsInN1YiI6IjYxZjUyYmJlMTA5MjMwMDA0MjBlZDJlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hobgu-ou3pVnuJT821tbglfBtOnCCFPKAJK4Veth8qo"
 };
