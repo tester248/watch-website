bbdfbf34173282ad587a2c74dcaa90c0a37e485d
tester248 <74985969+tester248@users.noreply.github.com>
2023-07-07 17:22:33 +0000

    fx

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "httphandler.deezgames.workers.dev",
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYzFmODcxMmE4MTMzM2Y5MDVjZmE0ZGVjNjhlY2JlNiIsInN1YiI6IjYxZjUyYmJlMTA5MjMwMDA0MjBlZDJlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hobgu-ou3pVnuJT821tbglfBtOnCCFPKAJK4Veth8qo"
+  VITE_CORS_PROXY_URL: "https://httphandler.deezgames.workers.dev",
+  VITE_TMDB_READ_API_KEY: "1c1f8712a81333f905cfa4dec68ecbe6"
 };
