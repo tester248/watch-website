8374a030908507a4a4398973ad95ceabc326a196
tester248 <74985969+tester248@users.noreply.github.com>
2023-07-26 19:44:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://httphandler.deezgames.workers.dev",
-  VITE_TMDB_READ_API_KEY: "b030404650f279792a8d3287232358e3",
-  VITE_TMDB_API_KEY: "b030404650f279792a8d3287232358e3",
+  VITE_TMDB_READ_API_KEY: "b267ab7d52674c23af4e4e6cf2956251",
+  VITE_TMDB_API_KEY: "b267ab7d52674c23af4e4e6cf2956251",
   VITE_OMDB_API_KEY: "aa0937c0",
 };
